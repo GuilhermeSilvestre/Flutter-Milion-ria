1e66569870598a6a5bccb075c170b48680c0d3aa
Guilherme Silvestre <gaasilvestre@gmail.com>
2024-05-03 23:01:23 -0300

    Fazendo tela Regitrar Jogo

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Milionária
 
 Projeto voltado ao jogo da loteria da Caixa Econômica Federal chamado **Mais Milionária**
+
+
+API:
+
+https://github.com/guto-alves/loterias-api?tab=readme-ov-file
+
+Endpoint dos resultados:
+
+https://loteriascaixa-api.herokuapp.com/api/maismilionaria/
+
+O Modelo dos resultados pode ser encontrado em **resultados.json**
--- lib/pages/registrarJogo.dart
+++ lib/pages/registrarJogo.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:milionaria/pages/perfil.dart';
 
 class RegistrarJogo extends StatefulWidget {
   const RegistrarJogo({Key? key}) : super(key: key);
@@ -8,6 +12,17 @@ class RegistrarJogo extends StatefulWidget {
 }
 
 class _RegistrarJogoState extends State<RegistrarJogo> {
+  final _formKey = GlobalKey<FormState>();
+  final _concursoController = TextEditingController();
+  final _numerosController = List<TextEditingController>.generate(
+    6,
+    (index) => TextEditingController(),
+  );
+  final _numerosExtrasController = List<TextEditingController>.generate(
+    2,
+    (index) => TextEditingController(),
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,13 +34,144 @@ class _RegistrarJogoState extends State<RegistrarJogo> {
             colors: [Color(0xFF2E3078), Colors.white],
           ),
         ),
-        child: const Center(
-          child: Text(
-            'Conteúdo da página REGISTRAR JOGO aqui',
-            style: TextStyle(fontSize: 20),
+        padding: const EdgeInsets.all(20.0),
+        child: Form(
+          key: _formKey,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                TextFormField(
+                  controller: _concursoController,
+                  decoration: const InputDecoration(
+                    labelText: 'Número do Concurso',
+                    labelStyle: TextStyle(color: Colors.white),
+                  ),
+                  keyboardType: TextInputType.number,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Por favor, insira o número do concurso';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 20.0),
+                const Text(
+                  'Números de 1 a 50:',
+                  style: TextStyle(fontSize: 16.0, color: Colors.white),
+                ),
+                const SizedBox(height: 5.0),
+                Wrap(
+                  spacing: 10.0,
+                  runSpacing: 10.0,
+                  children: List.generate(
+                    6,
+                    (index) => SizedBox(
+                      width: 60.0,
+                      child: TextFormField(
+                        controller: _numerosController[index],
+                        decoration: InputDecoration(
+                          labelStyle: const TextStyle(color: Colors.white),
+                          labelText: 'Nº ${index + 1}',
+                        ),
+                        keyboardType: TextInputType.number,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Por favor, insira um número';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20.0),
+                const Text(
+                  'Trevos de 1 a 6:',
+                  style: TextStyle(fontSize: 16.0, color: Colors.white),
+                ),
+                const SizedBox(height: 5.0),
+                Wrap(
+                  spacing: 10.0,
+                  runSpacing: 10.0,
+                  children: List.generate(
+                    2,
+                    (index) => SizedBox(
+                      width: 60.0,
+                      child: TextFormField(
+                        controller: _numerosExtrasController[index],
+                        decoration: InputDecoration(
+                          labelText: 'Nº ${index + 1}',
+                          labelStyle: const TextStyle(color: Colors.white),
+                        ),
+                        keyboardType: TextInputType.number,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Por favor, insira um número';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20.0),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      _saveJogo();
+                    }
+                  },
+                  child: const Text('Registrar Jogo'),
+                ),
+                const SizedBox(height: 20.0),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const Perfil()),
+                    );
+                  },
+                  child: const Text('Consultar jogos registrados'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
+  }
+
+  void _saveJogo() async {
+    final Map<String, dynamic> jogoData = {
+      'concurso': _concursoController.text,
+      'numeros': _numerosController
+          .map((controller) => int.parse(controller.text))
+          .toList(),
+      'numerosExtras': _numerosExtrasController
+          .map((controller) => int.parse(controller.text))
+          .toList(),
+    };
+
+    final File file = File('assets/jogosdousuario/jogosdousuario.json');
+    List<Map<String, dynamic>> jogos = [];
+    if (await file.exists()) {
+      final String fileContent = await file.readAsString();
+      jogos = json.decode(fileContent);
+    }
+
+    jogos.add(jogoData);
+    await file.writeAsString(json.encode(jogos));
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text('Jogo registrado com sucesso!'),
+      ),
+    );
+
+    // Limpar campos após salvar
+    _concursoController.clear();
+    _numerosController.forEach((controller) => controller.clear());
+    _numerosExtrasController.forEach((controller) => controller.clear());
   }
 }
